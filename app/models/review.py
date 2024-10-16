d4c694e3379768e45912e8e12dea767aab17a0f0
Shakil_xBCC <shakilmrf8@gmail.com>
2024-10-16 12:16:18 +0600

    added models

---
+++ app/models/review.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.core.database import Base
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String)
+    rating = Column(Integer)
+    product_id = Column(Integer, ForeignKey('products.id'))
+    product = relationship("Product")
